27c25452edcd5d88540151bbbfdeaf26d49c6154
smatsushima1 <smatsushima1@gmail.com>
2021-02-07 23:38:18 -0500

    add class for each paragraph

--- src/json_parse/dev02.py
+++ src/json_parse/dev02.py
@@ -7,33 +7,118 @@ import urllib as ul
 import re
 
 
-html = open('html/css_search.html', 'r').read()
-# Turn it into html and parse out the content
-soup = bsp(html, 'html.parser')
-htext = soup.find_all('style')
-list1 = []
-for i in htext:
-    for j in i:
-        list1.append(j)
-list2 = []
-for k in list1:
-    res = k.split('\n')
-    for l in res:
-        if l == '':
-            continue
-        else:
-            list2.append(l)
-print(list2)
+# html = open('html/css_search.html', 'r').read()
+# # Turn it into html and parse out the content
+# soup = bsp(html, 'html.parser')
+# htext = soup.find_all('style')
+# list1 = []
+# for i in htext:
+#     for j in i:
+#         list1.append(j)
+# list2 = []
+# for k in list1:
+#     res = k.split('\n')
+#     for l in res:
+#         if l == '':
+#             continue
+#         else:
+#             list2.append(l)
+# print(list2)
 
+# str1 = 'a'
+# while True:
+#     str1.isalpha()
+#     try:
+#         print(True)
+#         break
+#     except:
+#         print(False)
+#         break
+    
 #print(htext)
 
 # htext = soup.find('div', class_ = 'reg-container clearfix')
 # res = htext.find_all('a')
 
+from dev03 import PCitation
+num = ['a', 'b', 'c', 1, 2, 3, 'd', 'e', 1, 'i', 'f']
+# print(len(num))
+# print(num[11])
+cdev = PCitation(num)
+for x, i in enumerate(num):
+    cdev.get_attributes(x)
+
+# lnum = 7
+# print(num[:lnum])
+
+# prev_lalpha = False
+# for x, i in reversed(list(enumerate(num[:(lnum - 1)]))):
+#     # First iteration of the loop assumes no alpha character
+#     if x == 0:
+#         prev_lalpha = False
+#     # Break the loop here so that it stop repeating
+#     elif prev_lalpha == True:
+#         break
+#     while True:
+#         # pletter will only be saved if it runs true
+#         try:
+#             i.isalpha()  
+#             prev_lalpha = True
+#             pletter = i
+#             break
+#         except:
+#             prev_lalpha = False
+#             pletter = ''
+#             break
+#     print(str(x) + ' - ' + str(i) + ' - ' + str(pletter) + ' - ' + str(prev_lalpha))
+#     if prev_lalpha == True:
+#         break
+#     else:
+#         continue
+    
+# print(str(x) + ' - ' + str(i) + ' - ' + str(pletter) + str(prev_lalpha))
+
+
+# x = 'i'
 
 
 
 
+
+
+
+
+
+
+
+
+
+# for i in reversed(num[:2]):
+#     print(i)
+
+
+
+
+# for x, i in enumerate(reversed(num[:lnum])):
+# # for x, i in enumerate(num):
+#     val = ''
+#     if x == 0:
+#         val_isalpha = False
+#     elif val_isalpha == True:
+#         break
+#     while True:
+#         str(i).isalpha()
+#         try:
+#             val_isalpha = True
+#             print(i + ': this is a letter')
+#             val = i
+#             break
+#         except:
+#             val_isalpha = False
+#             print('nope')
+#             break
+#     continue
+# print(val)
 
 
 

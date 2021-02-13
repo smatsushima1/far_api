ddc79efbc098c308ad4187da89cb56a9b66f1538
smatsushima1 <smatsushima1@gmailcom>
2021-02-13 18:58:16 -0500

    test if ; are required

--- src/app/js/dev01.js
+++ src/app/js/dev01.js
@@ -1,21 +1,20 @@
 
 function changeName() {
-    let dev_str = document.getElementById('demo');
+    let dev_str = document.getElementById('demo')
     dev_str.innerHTML = 'Hello JavaScript!'
     };
 
 function popPart() {
-    var partDD = document.getElementById('dd_part');
+    var partDD = document.getElementById('dd_part')
     for (var i = 1; i <= 53; i++) {
-        var lst = document.createElement('option');
-        lst.innerHTML = i;
-        lst.value = i;
-        partDD.appendChild(lst);
-        };
+        var lst = document.createElement('option')
+        lst.innerHTML = i
+        lst.value = i
+        partDD.appendChild(lst)
+        }
     };
 popPart();
 
-last test
 
 function popSubpart() {
 

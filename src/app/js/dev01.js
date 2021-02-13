9a5e7ba093e93b5fb54ad99d1baea64981a72186
smatsushima1 <smatsushima1@gmail.com>
2021-02-13 15:22:11 -0500

    update index and dev01

--- src/app/js/dev01.js
+++ src/app/js/dev01.js
@@ -1,6 +1,23 @@
 
-function changeName(){
-    let dev_str = document.getElementById("demo");
-    dev_str.innerHTML = "Hello JavaScript!"
-}
+function changeName() {
+    let dev_str = document.getElementById('demo');
+    dev_str.innerHTML = 'Hello JavaScript!'
+    };
 
+function popPart() {
+    var partDD = document.getElementById('dd_part');
+    for (var i = 1; i <= 53; i++) {
+        var lst = document.createElement('option');
+        lst.textContent = i;
+        lst.value = i;
+        partDD.appendChild(lst);
+        };
+    };
+
+function popSubpart() {
+
+
+
+
+
+    };

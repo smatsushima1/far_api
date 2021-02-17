939f53df2ddb93f5c6f614611f40ae16351c4f2d
smatsushima1 <smatsushima1@gmail.com>
2021-02-16 19:12:10 -0500

    restructure everything 2

--- src/app/__main__.py
+++ src/app/__main__.py
@@ -1,7 +1,11 @@
 
 from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__,
+            static_url_path='',
+            static_folder='js',
+            template_folder='templates')
+
 @app.route('/')
 
 def index():
--- src/app/js/json/convert_json.py
+++ src/app/js/json/convert_json.py
@@ -3,7 +3,7 @@ import json
 doc = open('C:/Users/smats/git/far_api/src/app/js/json/parts.json', 'r', encoding = 'utf8').read()
 d1 = json.loads(doc)
 doc2 = open('C:/Users/smats/git/far_api/src/app/js/json/parts2.json', 'w', encoding = 'utf8')
-json.dump(d1, doc2, indent = 2)
+json.dump(d1, doc2)
 
 
 

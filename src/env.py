c1f03a5e3472e9d75e5203f7d0d9b7d66b072841
smatsushima1 <smatsushima1@gmail.com>
2021-02-24 17:40:11 -0500

    add environment variables

---
+++ src/env.py
@@ -0,0 +1,10 @@
+
+import os
+
+PG_USER = os.environ.get('PGUSER')
+PG_NAME = os.environ.get('PGDATABASE')
+PG_PW = os.environ.get('PGPASSWORD')
+PG_HOST = os.environ.get('PGHOST')
+PG_PORT = os.environ.get('PGPORT')
+
+
